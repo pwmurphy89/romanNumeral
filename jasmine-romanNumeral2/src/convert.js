f26b7c62c64006a528151faa9d0d9dcb25fc0a5c
pwmurphy89 <pwmurphy89@gmail.com>
2016-06-30 14:00:34 -0400

    unit tests

---
+++ jasmine-romanNumeral2/src/convert.js
@@ -0,0 +1,24 @@
+function makeMeRoman(number){
+	var romanString = "";
+	var romanArray = [
+		{number: 1000, character: 'M', specialNum: 900, specialChar: 'CM'},
+		{number: 500, character: 'D', specialNum: 400, specialChar: 'CD'},
+		{number: 100, character: 'C', specialNum: 90, specialChar: 'XC'},
+		{number: 50, character: 'L', specialNum: 40, specialChar: 'XL'},
+		{number: 10, character: 'X', specialNum: 9, specialChar: 'IX'},
+		{number: 5, character: 'V', specialNum: 4, specialChar: 'IV'},
+		{number: 1, character: 'I', specialNum: 0, specialChar: null}
+	];
+	for(var i=0; i<romanArray.length;i++){
+		var numberOfNumeral = Math.floor(number / romanArray[i].number);
+		for (var j=0; j<numberOfNumeral;j++){
+			romanString += romanArray[i].character;
+		}
+		number = number - numberOfNumeral * romanArray[i].number; 
+		if(number/ romanArray[i].specialNum >=1){
+			romanString += romanArray[i].specialChar;
+			number -= romanArray[i].specialNum;
+		}
+	}
+	return romanString;
+}
