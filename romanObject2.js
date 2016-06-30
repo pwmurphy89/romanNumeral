d2add7d0c387d8bb4b362482003de4c521af8e84
pwmurphy89 <pwmurphy89@gmail.com>
2016-06-30 13:46:37 -0400

    refactored further

--- romanObject2.js
+++ romanObject2.js
@@ -1,5 +1,4 @@
 function makeMeRoman(number){
-	var number = number;
 	var romanString = "";
 	var romanArray = [
 		{number: 1000, character: 'M', specialNum: 900, specialChar: 'CM'},
@@ -10,19 +9,18 @@ function makeMeRoman(number){
 		{number: 5, character: 'V', specialNum: 4, specialChar: 'IV'},
 		{number: 1, character: 'I', specialNum: 0, specialChar: null}
 	];
-	function buildRomanString(amount, romanObj){
-		var numberOfNumeral = Math.floor(amount / romanObj.number);
-		for (var i=0; i<numberOfNumeral;i++){
-			romanString += romanObj.character;
-		}
-		number = amount - numberOfNumeral * romanObj.number; 
-		if(number/ romanObj.specialNum >=1){
-			romanString += romanObj.specialChar;
-			number -= romanObj.specialNum;
-		}
-	}
 	for(var i=0; i<romanArray.length;i++){
-		buildRomanString(number,romanArray[i]);
+		var numberOfNumeral = Math.floor(number / romanArray[i].number);
+		for (var j=0; j<numberOfNumeral;j++){
+			romanString += romanArray[i].character;
+		}
+		number = number - numberOfNumeral * romanArray[i].number; 
+		if(number/ romanArray[i].specialNum >=1){
+			romanString += romanArray[i].specialChar;
+			number -= romanArray[i].specialNum;
+		}
 	}
 	return romanString;
 }
+
+makeMeRoman(111);
